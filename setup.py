488623fe093ce9b79e5cd2561f0535acf95463b6
samlikesphysics <sloomis@ucdavis.edu>
2021-10-08 14:42:49 -0700

    0.1.6 active

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='stoclust',
-    version='0.1.5',
+    version='0.1.6',
     author='Samuel Loomis',
     author_email='sloomis@ucdavis.edu',
     packages=['stoclust'],
     description='Modular methods for stochastic clustering',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'numpy >= 1.15.0',
         'scipy >= 1.1.0',
